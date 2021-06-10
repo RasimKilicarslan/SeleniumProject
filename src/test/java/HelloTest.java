70c1255f9eaa0c31fe18f306d88350ed96a80418
RasimKilicarslan <kilicarslanrasim2002@gmail.com>
2021-06-10 15:08:30 +0100

    initial commit

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class HelloTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello test");
+    }
+}
